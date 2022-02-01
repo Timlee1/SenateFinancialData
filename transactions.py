83004a12df925709697a4d859cce0cfb666ad176
Timlee1 <71849482+Timlee1@users.noreply.github.com>
2022-02-01 00:42:35 -0500

    Data cleaning

--- transactions.py
+++ transactions.py
@@ -4,12 +4,26 @@ import pandas as pd
 
 df = pd.read_csv('raw_data.csv')
 
+
+def strip_td(s):
+    start = s.find(">")
+    end = s.find("<", start)
+    return s[start+1:end]
+
+
+for ind in df.index:
+    df['First'][ind] = strip_td(df['First'][ind]).upper()
+    df['Last'][ind] = strip_td(df['Last'][ind]).upper()
+    df['Office'][ind] = strip_td(df['Office'][ind])
+    df['Date'][ind] = strip_td(df['Date'][ind])
+
+
 df['Report Type'] = df['Report']
 
 
 for ind in df.index:
     s = df['Report'][ind]
-    if (s.find('paper') != -1) or (s.find('Amendment') != -1):
+    if (s.find('paper') != -1) or (s.find('Amendment') != -1) or (s.find('Extension') != -1) or (s.find('Blind Trust') != -1):
         df.drop(ind, inplace=True)
 
 for ind in df.index:
@@ -21,6 +35,10 @@ for ind in df.index:
     end_type = s.find("<", end)
     sub_type = s[end+1:end_type]
     df['Report Type'][ind] = sub_type
+    indf = sub_type.find('for')
+    if indf != -1:
+        end = indf-1
+        df['Report Type'][ind] = sub_type[0:end]
 
 
 df['Date'] = pd.to_datetime(df['Date'])
